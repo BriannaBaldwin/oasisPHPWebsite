50d262c5f263983b47348498e0d92af775cbd8df
Brianna Baldwin <73656451+BriannaBaldwin@users.noreply.github.com>
2021-09-17 10:39:43 -0600

    Updated and Added files
    created modal file and cleaned up database access. Edited nav links

---
+++ admin.php
@@ -0,0 +1,142 @@
+<?php
+//Original Author: Brianna Baldwin
+//Date Created: 09/10/2021
+//Version: 0.0
+//Date Last Modified: 09/10/2021
+//Modified by: Brianna Baldwin
+//Modification log:
+//   09/10/2021 - created admin.php | added php to collect and post messages for each assigned volunteer | added option to delete messages
+//   09/17/2021 - linked model database.php and volunteer.php files | deleted database link | add volunteer and visit functions | added nav links
+
+require_once ('./model/database.php');
+require_once ('./model/volunteer.php');
+require_once ('./model/submission.php');
+
+//Check action; on initial load it is null
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_submissions';
+    }
+}
+
+if ($action == 'list_submissions') {
+
+
+//See if volunteer is set
+    $volunteer_id = filter_input(INPUT_GET, 'volunteer_id', FILTER_VALIDATE_INT);
+    if ($volunteer_id == NULL || $volunteer_id == FALSE) {
+        $volunteer_id = 1;
+    }
+
+    try {
+        $volunteers = VolunteerDB::getVolunteerList();
+        $submissions = getSubmissionByEmp($volunteer_id);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+} else if ($action == 'delete_submission') {
+    delSubmission($submission_id);
+    header("Location: admin.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contact Page</title>
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/contact.css">
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon_io/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="img/favicon_io/favicon-32x32.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="img/favicon_io/apple-touch-icon.png">
+        <link rel="icon" sizes="192x192" href="img/favicon_io/android-chrome-192x192.png">
+        <!-- JS Scripts -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+        <script src="js/hambMenu.js"></script> <!-- JS for HambMenu -->
+    </head>
+    <header>
+            <!-- <img src="img/drop.png" alt="Oasis Droplet Logo" height="30px" width="30px"> -->
+        <!-- Navbar -->
+        <nav>
+            <a name="top"></a>
+            <ul class="topnav" id="myTopnav">
+                <div>
+                    <li><a href="home.html" class="active">Oasis</a></li>
+                    <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+                    <li><a href="javascript:void(0);" id="hambNav" class="hambNav" onclick="hambFunction()">
+                            <span><i class="fa fa-bars"><img id="hambImg" class="rotate" src="img/logo2.png"/></i></span>
+                        </a></li>
+                </div>
+                <!-- Navigation links (hidden by default) -->
+                <div id="myLinks" class="">
+                   <li class="item"><a href="home.html">Slideshow</a></li>
+                    <li class="item"><a href="home.html">Newsletter</a></li>
+                    <li class="item"><a href="home.html">FAQs</a></li>
+                    <li class="item"><a href="contact.html">Contact</a></li>
+                    <li class="item"><a href="admin.php">Admin</a></li>
+                    <li class="item"><a href="list_volunteers.php">ListVol</a></li>
+                </div>  
+            </ul>
+        </nav>
+    </header>
+    <body>
+        <main>
+            <div id="" class="">
+                <h1>Admin</h1> 
+                <h3>Select a volunteer to view messages</h3>
+                <aside>
+                    <ul style="list-style-type:none; ">
+                        <?php foreach ($volunteers as $volunteer) : ?>
+                            <li>
+                                <a href="?volunteer_id=<?php echo $volunteer['volunteer_id']; ?>">
+                                    <?php echo $volunteer['first_name'] . ' ' . $volunteer['last_name']; ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </aside>
+                <table>
+                    <tr>
+                        <th>Date</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Contact By</th>
+                        <th>Comments</th>
+                        <th></th>
+                    </tr>
+                    <?php foreach ($submissions as $submission) : ?>
+                        <tr>
+                            <td><?php echo $submission['submission_date']; ?></td>
+                            <td><?php echo $submission['email']; ?></td>
+                            <td><?php echo $submission['phone']; ?></td>
+                            <td><?php echo $submission['contact_by']; ?></td>
+                            <td><?php echo $submission['comments']; ?></td>
+                            <td>
+                                <form action="admin.php" method="post">
+                                    <input type="hidden" name="action" value="delete_submission" />
+                                    <input type="hidden" name="submission_id" 
+                                           value="<?php echo $submission['submission_id']; ?>" />
+                                    <input type="submit" value="Delete" class="button"/>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        </main>
+    </body>
+    <footer>
+        <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a><br>
+            <p>Inspiration from <a href="https://us.wateraid.org/campaign/emergency-global-hygiene-fund/c277193?c_src=ig-dig-20b-cvd&c_src2=bra-7&gclid=Cj0KCQiAjKqABhDLARIsABbJrGl53kVQKVAFT1Pg7v0M06_lnRx8zaOuMVF8BXxjtwbCUdUS9H8XtvwaAt1cEALw_wcB"> WaterAid.com</a></p>
+            <p>&copy; Copyright 2021. All Rights Reserved.</p>
+        </div>
+    </footer>
+</html>
---
+++ oasis.sql
@@ -0,0 +1,235 @@
+#****************************************************
+# Script to create oasis database and add test data. 
+# Name 	Date  		Description
+# Brianna	8/27/2021	Initial deployment
+#
+#****************************************************
+DROP DATABASE IF EXISTS oasis;
+CREATE DATABASE oasis;
+
+USE oasis;
+
+# create objects
+CREATE TABLE IF NOT EXISTS volunteer
+(
+	volunteer_id	INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	first_name 	VARCHAR(60) NOT NULL,
+	last_name	VARCHAR(60) NOT NULL,
+	email_address 	VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS form_submission
+(
+	submission_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,	
+	email	VARCHAR(100) NOT NULL,
+	phone	VARCHAR(15) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	contact_by VARCHAR(50) NULL,
+	tos 	tinyint NOT NULL,	
+	comments VARCHAR (999) NOT NULL,
+	submission_date DATETIME NOT NULL,
+	volunteer_id	INT NOT NULL,
+	FOREIGN KEY (volunteer_id) REFERENCES volunteer(volunteer_id)
+);
+# insert statements for volunteer table
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Jane', 'Doe', 'janedoe@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Mickey', 'Mouse', 'mickeymouse@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Steven', 'Universe', 'stevenuniverse@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Jake', 'Dog', 'jakedog@oasis.com');
+
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Finn', 'Human', 'finnhuman@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Good', 'Boy', 'goodboy@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Red', 'Rover', 'redrover@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Lightning', 'McQuees', 'lightningmcqueen@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Bonnie', 'Bubblegum', 'bonniebubblegum@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Strawberry', 'Shortcake', 'strawberryshortcake@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('American', 'Eagle', 'americaneagle@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Ash', 'Ketchum', 'ashketchum@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Sloppy', 'Joe', 'sloppyjoe@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Buggs', 'Bunny', 'buggsbunny@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Michael', 'Jordan', 'michaeljordan@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Lola', 'Bunny', 'lolabunny@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('John', 'Snow', 'johnsnow@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Hiccup', 'Haddock', 'hiccuphaddocks@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Luke', 'Skywalker', 'lukeskywalker@oasis.com');
+	
+INSERT INTO volunteer
+	(first_name, last_name, email_address)
+VALUES
+	('Han', 'Solo', 'hansolo@oasis.com');
+	
+	
+
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('bobbyjoe@email.com', '1234567890', 'USA', 'phone', 1, 'Hello', NOW(), 1);
+
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('henriburns@email.com', '3344575883', 'USA', 'email', 1, 'I am henri', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('patienceharding@email.com', '8132153522', 'USA', 'text', 1, 'Good Job', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('ebrahimrhodes@email.com', '3603461787', 'Canada', 'phone', 0, 'Haha youve been hacked', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('nellabray@email.com', '3135328012', 'Canada', 'phone', 1, 'Im from canada', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('annalisesteele@email.com', '4193821120', 'Canada', 'dont contact', 1, 'dont try and call me', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('kerifuentes@email.com', '5183734800', 'Mexico', 'text', 1, 'im from mexico', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('kylaochoa@email.com', '4346658071', 'Mexico', 'phone', 1, 'call me as soon as possible', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('chloecampos@email.com', '6185599381', 'Mexico', 'phone', 1, 'i have a computer', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('rhiannanguzman@email.com', '6304357272', 'USA', 'dont contact', 1, 'i want to watch Naruto', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('florencecamacho@email.com', '6463313428', 'USA', 'dont contact', 1, 'im over halfway there!', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('opalwarren@email.com', '7692201775', 'Mexico', 'email', 1, 'email only. i dont have a phone', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('kobytalbot@email.com', '4709997173', 'Canada', 'email', 1, 'i saw a moose yesterday', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('leannahume@email.com', '3233945667', 'Mexico', 'text', 1, 'im snorkeling so just text me', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('esagreaves@email.com', '4329784862', 'USA', 'dont contact', 1, 'i just wanted to send a message to someone', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('finnroach@email.com', '3349928035', 'Canada', 'text', 1, 'i want to live in mexico', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('mylieduncan@email.com', '8134056820', 'USA', 'phone', 1, 'im going dancing tonight', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('juiletsalter@email.com', '3368779559', 'USA', 'email', 0, 'hehehe i turned off javascript', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('millicentbegum@email.com', '7474007224', 'mexico', 'phone', 1, 'i want to live in the USA', NOW(), 1);
+	
+INSERT INTO form_submission
+	(email, phone, country, contact_by, tos, comments, submission_date, volunteer_id)
+VALUES
+	('sylviawood@email.com', '2093842634', 'USA', 'text', 1, 'im the last on the list', NOW(), 1);
